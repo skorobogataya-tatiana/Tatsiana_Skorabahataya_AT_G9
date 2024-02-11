a113733ff84030eee92c2d0149ea7160058a50e5
Tatsiana Skorabahataya <skorobogataya.tatiana@gmail.com>
2024-02-11 10:36:47 +0300

    Adjust water class

--- src/project/content/Water.java
+++ src/project/content/Water.java
@@ -1,12 +1,12 @@
 package project.content;
 
 public abstract class Water {
-    private String color;
-    private String smell;
-    private double transperency;
-    private double temperature;
+    private String color = "no";
+    private String smell = "no";
+    private String transperency = "transparent";
+    private int temperature = 0;
 
-    public Water(String color, String smell, double transperency, double temperature) {
+    public Water(String color, String smell, String transperency, int temperature) {
         this.color = color;
         this.smell = smell;
         this.transperency = transperency;
@@ -23,12 +23,12 @@ public abstract class Water {
         return smell;
     }
 
-    public double getTransperency() {
+    public String getTransperency() {
 
         return transperency;
     }
 
-    public double getTemperature() {
+    public int getTemperature() {
 
         return temperature;
     }
@@ -42,12 +42,12 @@ public abstract class Water {
         this.smell = smell;
     }
 
-    public void setTransperency(double transperency) {
+    public void setTransperency(String transperency) {
 
         this.transperency = transperency;
     }
 
-    public void setTemperatre(double temperature) {
+    public void setTemperatre(int temperature) {
 
         this.temperature = temperature;
     }
