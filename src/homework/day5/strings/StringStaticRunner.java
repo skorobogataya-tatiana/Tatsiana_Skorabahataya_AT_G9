763e5f93cdcf7e76a1006f99db9d880da992a545
Tatsiana Skorabahataya <skorobogataya.tatiana@gmail.com>
2024-02-17 19:57:40 +0300

    Add adjustments for duplicates searching method

--- src/homework/day5/strings/StringStaticRunner.java
+++ src/homework/day5/strings/StringStaticRunner.java
@@ -5,7 +5,7 @@ public class StringStaticRunner {
     public static void main(String[] args) {
 
         StringAsArray.showLettersOfString("This is my string!");
-        Duplicates.showDuplicates("This duplicates string is a string for checking this string duplicates string !");
+        Duplicates.showDuplicates("This duplicates string is a string for checking this string duplicates string!");
         NumbersInString.showNumbersFromString("This is string 1 that contains 234 73 some numbers 09 ");
 
         ParceLog.checkLog("access_log.2020.09.07 212.168.101.5 granted\n" +
