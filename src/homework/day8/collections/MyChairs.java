4dc7e9982b468f7cdd6ff0ad1bbd38943494b093
Tatsiana Skorabahataya <skorobogataya.tatiana@gmail.com>
2024-02-26 21:11:29 +0300

    Re-named array list of chairs to "furniture"

--- src/homework/day8/collections/MyChairs.java
+++ src/homework/day8/collections/MyChairs.java
@@ -11,19 +11,19 @@ public class MyChairs {
 
     public static void main(String[] args) {
 
-        List<Chair> chairs = Arrays.asList(new Chair(1, 2),
+        List<Chair> furniture = Arrays.asList(new Chair(1, 2),
                 new Chair(4, 2),
                 new Chair(2, 4));
 
-        for (Chair someChair : chairs) {
+        for (Chair someChair : furniture) {
             int heightMyltiplyPerWidth = someChair.getWidth() * someChair.getHeight();
             System.out.print(heightMyltiplyPerWidth + " ");
         }
         System.out.println();
 
         Map<Integer, Chair> myChairMap = new HashMap<>();
-        for (int i = 0; i < chairs.size(); i++) {
-            myChairMap.put(i, chairs.get(i));
+        for (int i = 0; i < furniture.size(); i++) {
+            myChairMap.put(i, furniture.get(i));
         }
 
         System.out.print("The keys of the chair map: ");
