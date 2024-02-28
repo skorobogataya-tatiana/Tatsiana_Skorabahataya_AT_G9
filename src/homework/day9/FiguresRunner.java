738a04db79c3d5d42ce9a3917c001b1e1aa95bb1
Tatsiana Skorabahataya <skorobogataya.tatiana@gmail.com>
2024-02-28 13:28:29 +0300

    Adjust figures runner

--- src/homework/day9/FiguresRunner.java
+++ src/homework/day9/FiguresRunner.java
@@ -6,6 +6,6 @@ public class FiguresRunner {
     public static void main(String[] args) {
         Stream<String> figures = Stream.of("Овал", "Прямоугольник", "Круг", "Квадрат", "Эллипс");
 
-        figures.mapToInt(String::length).filter(i -> i > 4).mapToObj(String::valueOf).forEachOrdered(System.out::println);
+        figures.mapToInt(String::length).filter(i -> i > 4).forEachOrdered(System.out::println);
     }
 }
