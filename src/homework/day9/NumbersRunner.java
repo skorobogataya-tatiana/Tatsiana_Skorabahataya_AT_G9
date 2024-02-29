4d1ff903ab17f9ae9047b6f0435da01579781517
Tatsiana Skorabahataya <skorobogataya.tatiana@gmail.com>
2024-02-29 11:42:56 +0300

    Add numbers runner

---
+++ src/homework/day9/NumbersRunner.java
@@ -0,0 +1,12 @@
+package homework.day9;
+
+import java.util.stream.Stream;
+
+public class NumbersRunner {
+    public static void main(String[] args) {
+
+        Stream<Integer> numbers = Stream.of(3342, 34, 79, 23426, 68, 1324, 55, 7699);
+        System.out.print(numbers.peek(System.out::println).sorted().peek(System.out::println).reduce(0, Integer::sum));
+
+    }
+}
