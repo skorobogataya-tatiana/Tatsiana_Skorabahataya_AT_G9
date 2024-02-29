7dc6441d6b6985ed27e5ee735acdfe0d81d5df0f
Tatsiana Skorabahataya <skorobogataya.tatiana@gmail.com>
2024-02-29 16:38:41 +0300

    Adjust iteration of elements stream

--- src/homework/day9/ElementsRunner.java
+++ src/homework/day9/ElementsRunner.java
@@ -7,6 +7,7 @@ public class ElementsRunner {
     public static void main(String[] args) {
         Stream<String> elements = Stream.of("Text field", "Radio", "Check-box", "Drop-down", "Picker", "Breadcrumb");
 
+        //Version for considering each element of the stream as one name
         elements.map(s -> {
             int index = IntStream.range(0, 6).filter(i -> Stream.of("Text field", "Radio", "Check-box", "Drop-down", "Picker", "Breadcrumb")
                     .toArray()[i]
@@ -18,5 +19,22 @@ public class ElementsRunner {
                 return String.valueOf(s.length());
             }
         }).distinct().forEach(System.out::println);
+        System.out.println();
+
+        //Version when we need to iterate through each word of each element of the stream
+        Stream<String> elements2 = Stream.of("Text field", "Radio", "Check-box", "Drop-down", "Picker", "Breadcrumb");
+        elements2.map(s -> {
+                    String[] nameWords = s.split("[\\s-]");
+                    String newName = "";
+                    for (int i = 0; i < nameWords.length; i++) {
+                        if (i % 2 == 0) {
+                            newName += nameWords[i].replaceAll("e",  "a") + " ";;
+                        } else {
+                            newName += nameWords[i].length() + " ";
+                        }
+                    }
+                    return newName.trim();
+                }
+        ).distinct().forEach(System.out::println);
     }
 }
