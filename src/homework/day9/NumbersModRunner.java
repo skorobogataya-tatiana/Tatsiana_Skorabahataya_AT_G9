c784060e68823a7202c7588a1820d2218db396eb
Tatsiana Skorabahataya <skorobogataya.tatiana@gmail.com>
2024-02-29 12:42:40 +0300

    Add numbersMod runner

---
+++ src/homework/day9/NumbersModRunner.java
@@ -0,0 +1,36 @@
+package homework.day9;
+
+import java.util.stream.Stream;
+
+public class NumbersModRunner {
+    public static void main(String[] args) {
+        Stream<Integer> numbersMod = Stream.of(626, 435, 9, 1463268, 24, 2237, 33, 9090);
+        numbersMod.filter(i -> i.toString().contains("3")).flatMapToInt(i -> String.valueOf(i).chars()).mapToObj(c -> (char) c).
+                map(c -> {
+                    switch (c) {
+                        case '0':
+                            return "Zero";
+                        case '1':
+                            return "One";
+                        case '2':
+                            return "Two";
+                        case '3':
+                            return "Three";
+                        case '4':
+                            return "Four";
+                        case '5':
+                            return "Five";
+                        case '6':
+                            return "Six";
+                        case '7':
+                            return "Seven";
+                        case '8':
+                            return "Eight";
+                        case '9':
+                            return "Nine";
+                        default:
+                            return "";
+                    }
+                }).forEach(System.out::println);
+    }
+}
