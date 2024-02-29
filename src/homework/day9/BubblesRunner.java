5967b390d633074b0a039f99584e109c0cf45840
Tatsiana Skorabahataya <skorobogataya.tatiana@gmail.com>
2024-02-29 17:16:43 +0300

    Add runner for bubbles

---
+++ src/homework/day9/BubblesRunner.java
@@ -0,0 +1,20 @@
+package homework.day9;
+
+import homework.day8.practice.Bubble;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+
+public class BubblesRunner {
+    public static void main(String[] args) {
+        Collection<Bubble> bubbles = Arrays.asList(new Bubble(2, "CO2"),
+                new Bubble(4, "O2"),
+                new Bubble(5, "CO"));
+
+        System.out.println(bubbles.stream().filter(b -> b.getVolume() > 3)
+                .sorted(Comparator.comparing(Bubble::getName))
+                .map(b -> new Bubble(b.getVolume() * 3, b.getName()))
+                .mapToInt(Bubble::getVolume).reduce(Integer::sum).getAsInt());
+    }
+}
