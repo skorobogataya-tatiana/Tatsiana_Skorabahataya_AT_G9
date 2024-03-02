28979a5a7220ad0e7a66b385884d2a93119b640e
Tatsiana Skorabahataya <skorobogataya.tatiana@gmail.com>
2024-03-02 15:30:42 +0300

    Classwork practice day 10

---
+++ src/classwork/day10/Person.java
@@ -0,0 +1,46 @@
+package classwork.day10;
+
+public class Person {
+    public String name;
+    public int age;
+    public Sex sex;
+
+    public Person(String name, int age, Sex sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", sex=" + sex +
+                '}';
+    }
+
+    public enum Sex {
+        MAN, WOMEN
+    }
+}
